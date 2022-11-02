86030bbcbc382d23a339eb774a332f43d189f669
@chirayuchaudhari <chirayuchaudhari@gmail.com>
2022-11-02 12:55:45 +0530

    project commit

---
+++ src/main/java/com/chirayu/security/PasswordService.java
@@ -0,0 +1,28 @@
+package com.chirayu.security;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+	public static String encode(String txt) {
+		Encoder encoder=Base64.getEncoder();
+		return encoder.encodeToString(txt.getBytes());
+	}
+	
+	public static String decode(String encodetxt) {
+		Decoder decoder=Base64.getDecoder();
+		byte[] decode=decoder.decode(encodetxt);
+		return new String(decode);
+	}
+	
+	public static void main(String[] args) {
+		String encode=PasswordService.encode("chirayu");
+		System.out.println(encode);
+		
+		
+		String decode=PasswordService.decode(encode);
+		System.out.println(decode);
+	}
+
+}
